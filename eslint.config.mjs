0b280c3a6e94400b2d0330529447d1f5aab23b5c
pantho0 <panthomashreky6@gmail.com>
2024-11-04 15:01:15 +0600

    student module and student related route controller added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+// import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -22,5 +22,5 @@ export default [
 
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
+  // eslintPluginPrettierRecommended,
 ];
